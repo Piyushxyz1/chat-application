cefe4f32b18d5a2e64c9b7ef9c274bdbf0c487a3
piyushxyz1 <piyushnegi033@gmail.com>
2025-10-15 17:56:40 +0530

    changes made

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5173,
     proxy :{
 
-      // "/api":{
-      //   target:"http://localhost:8000"
-      // }
+      "/api":{
+        target:"http://localhost:8000"
+      }
     }
   }
 })
