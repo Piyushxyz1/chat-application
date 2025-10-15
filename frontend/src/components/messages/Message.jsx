570614ae2eef6076afdad99fdbd3950d0b7518be
piyushxyz1 <piyushnegi033@gmail.com>
2025-10-15 19:13:07 +0530

    changes made

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -23,7 +23,7 @@ const Message = ({message}) => {
 					<img alt='Tailwind CSS chat bubble component' src={profilePic} />
 				</div>
 			</div>
-			<div className={`chat-bubble text-[#000] ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
+			<div className={`chat-bubble text-[#000] bg-[#fff] ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
 			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
 		</div>
 	);
