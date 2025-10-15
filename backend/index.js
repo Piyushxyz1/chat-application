95db138b36732446ed0d26fa472a670ce69a65be
piyushxyz1 <piyushnegi033@gmail.com>
2025-10-15 12:28:29 +0530

    deploymenting app

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,40 @@
 
+import path from 'path';
 import dotenv from 'dotenv'
 import express  from'express'
 import authRoutes from './routes/auth.Routes.js';
 import messageRoutes from "./routes/message.Routes.js";
 import userRoutes from "./routes/user.Routes.js"
-import mongoConn from './database/mongoDB.js'
 import cookieParser from 'cookie-parser'; 
+import mongoConn from './database/mongoDB.js'
 import { app,server } from './socket/socket.js';
 
 
-
-
-
 dotenv.config()
+const __dirname =  path.resolve();
+
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json())
 app.use(cookieParser())
 
 
-
-// app.get('/',(req,res)=>{
-
-// res.send("hello deepesh!!")
-
-// })
 
 
 app.use('/api/auth',authRoutes)   // using this middleware app.use for routes
 app.use('/api/messages',messageRoutes)
 app.use('/api/users',userRoutes)
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get("/*splat", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
-const PORT = process.env.PORT || 5000
+
+
+
  // creates an http server
 
 server.listen(PORT,()=>{    
