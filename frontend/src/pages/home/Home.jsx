193ab9e8ff6e1ea7d30745734cf9d2aa6209878b
piyushxyz1 <piyushnegi033@gmail.com>
2025-10-15 14:04:58 +0530

    changes made

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -5,7 +5,7 @@ import MessageContainer from '../../components/messages/MessageContainer'
 
 const Home = () => {
   return (
-    <div className='flex  sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-900'>
+    <div className='flex flex-col  sm:h-[450px]  md:h-[550px] rounded-lg overflow-hidden bg-gray-900'>
      <Sidebar/>
      <MessageContainer/>
 
